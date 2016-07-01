810ced04f2e07c184a487809fbc5b796b3b66d30
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2016-07-01 16:20:07 -0700

    Initial Commit

---
+++ Google Maps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Google Maps
+//
+//  Created by Walter Gonzalez Domenzain on 01/07/16.
+//  Copyright © 2016 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
